b5b7be9639a131dd739eefdfa9b4eab45786107d
zace3d <ed.zace90@gmail.com>
2015-05-28 19:22:33 -0500

    Mejor consusmo de memoria

--- app/src/main/java/mx/com/factico/diputinder/fragments/MainFragment.java
+++ app/src/main/java/mx/com/factico/diputinder/fragments/MainFragment.java
@@ -303,8 +303,10 @@ public class MainFragment extends Fragment implements View.OnClickListener {
                 @Override
                 public void onScroll(float scrollProgressPercent) {
                     View view = flingContainer.getSelectedView();
-                    view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
-                    view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+                    if (view != null) {
+                        view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                        view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+                    }
                 }
             });
 
